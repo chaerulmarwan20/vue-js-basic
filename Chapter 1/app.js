966fe68a87d10accf727def815447ced2965523f
Marwan <chaerul.marwan@weekendinc.com>
2021-11-14 13:02:17 +0700

    initial commit

---
+++ Chapter 1/app.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    maximum: 50,
+    products: null,
+    cart: [],
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((res) => res.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  methods: {
+    addItem: function (product) {
+      this.cart.push(product);
+    },
+  },
+});
---
+++ Chapter 2/app.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    slugText: "The Quick #(*!&@ Brown 29",
+  },
+  computed: {
+    slugtize: function () {
+      return (
+        this.slugText
+          .toLowerCase()
+          .replace(/[^\w ]+/g, "")
+          .replace(/ +/g, "-") +
+        "-" +
+        this.now()
+      );
+    },
+  },
+  methods: {
+    now: function () {
+      const date = new Date();
+      return (
+        String(date.getHours()) +
+        String(date.getMinutes()) +
+        String(date.getSeconds())
+      );
+    },
+  },
+});
